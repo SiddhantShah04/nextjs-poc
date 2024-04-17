7058007548a6419028a689389b24d72faa6526db
FIRST_NAME LAST_NAME <siddhantshah04@gmail.com>
2024-04-17 22:02:00 +0530

    tets

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -6,10 +6,11 @@ export default async function handler(req, res) {
 // Axios GET Default
 try {
     const data = await axios
-  .get("https://google.com");
-// console.log("data",data.data)
+  .get("http://34.143.197.93:5000/");
+console.log("data",data.data)
     res.status(200).json({
         "s":"Sdc",
+       data: data.data
     })
 } catch (error) {
     console.log(error)
